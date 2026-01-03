05613be24841a2ae04a7c9cf2ccd0cab082e0306
falkaer <kenny@falkaer.io>
2026-01-03 16:08:07 +0100

    fix paths

--- flake.nix
+++ flake.nix
@@ -1,11 +1,9 @@
 {
   description = "Pomerium prebuilt binaries + NixOS module using the flake package";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
   outputs =
     {
       self,
@@ -23,12 +21,10 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         version = "v0.31.3";
-
         x86_64-linux = pkgs.fetchurl {
           url = "https://github.com/pomerium/pomerium/releases/download/${version}/pomerium-linux-amd64.tar.gz";
           hash = "sha256-qXRWaTl/2y5aTbWMCNxRGstgfSaXR9BPvGmVv2tZWD8=";
         };
-
         aarch64-linux = pkgs.fetchurl {
           url = "https://github.com/pomerium/pomerium/releases/download/${version}/pomerium-linux-arm64.tar.gz";
           hash = "sha256-LT823iz3J2AK+4to6scn+1yLqyXB7oPJy7xUD5RXNAo=";
@@ -38,21 +34,17 @@
         packages.pomerium = pkgs.stdenv.mkDerivation {
           pname = "pomerium";
           inherit version;
-
           src = { inherit x86_64-linux aarch64-linux; }.${system};
           sourceRoot = ".";
-
           nativeBuildInputs = [ pkgs.autoPatchelfHook ];
           buildInputs = [ pkgs.stdenv.cc.cc.lib ];
-
           installPhase = ''
             runHook preInstall
             mkdir -p "$out/bin"
-            cp */pomerium "$out/bin/"
+            cp ./pomerium "$out/bin/pomerium"
             chmod +x "$out/bin/pomerium"
             runHook postInstall
           '';
-
           meta = with pkgs.lib; {
             description = "Pomerium - Identity-aware access proxy";
             homepage = "https://github.com/pomerium/pomerium";
@@ -64,9 +56,7 @@
             ];
           };
         };
-
         packages.default = self.packages.${system}.pomerium;
-
         apps.pomerium = flake-utils.lib.mkApp { drv = self.packages.${system}.pomerium; };
         apps.default = self.apps.${system}.pomerium;
       }
